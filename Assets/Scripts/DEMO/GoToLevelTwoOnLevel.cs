fda4d4064c85b7e5ef30e4e1c6bd397d5ae55ebf
LeiGGaRFeeD <ridefoot@gmail.com>
2023-04-30 14:52:58 +0300

    Add Demo Scripts
    Prepared for demo

--- Assets/Scripts/DEMO/GoToLevelTwoOnLevel.cs
+++ Assets/Scripts/DEMO/GoToLevelTwoOnLevel.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FinishPoint : MonoBehaviour
+public class GoToLevelTwoOnLevel : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<Signal>())
+        if (collision.GetComponent<Signal>())
         {
-            Debug.Log("Finished");
-            SceneManager.LoadScene("Finished");
+            Debug.Log("Load second level");
+            SceneManager.LoadScene("SCENENAME");
         }
-
     }
     // Update is called once per frame
     void Update()
